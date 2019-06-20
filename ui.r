f397db0f0fa7cf9b73cb6a22add22982b8ee97c1
robertshep1994 <43388314+robertshep1994@users.noreply.github.com>
2019-06-20 10:37:57 -0600

    Update ui.r

--- ui.r
+++ ui.r
@@ -7,10 +7,13 @@ ui <- fluidPage(
   #shinythemes::themeSelector(),
   titlePanel("S3 Cost Calculator"),
   
+  p("This cost calculator is intended to be used to estimate and compare the costs of storage, upload, and data retrieval for each of the S3 storage classes. The page tabs represent each of the S3 storage classes currently available in AWS."),
+  p("To begin, click one of the storage class tabs and input the information listed."),
+  
   tabsetPanel(type="tab",
-              
+                  
               tabPanel("Standard",br(),
-                       
+                       p("Standard is the default storage class which is designed for frequently accessed data."),
                        sidebarLayout(
                          
                          sidebarPanel(
@@ -76,7 +79,7 @@ ui <- fluidPage(
               ),
               
               tabPanel("Infrequent Access",br(),
-                       
+                       p("Infrequent Access is designed for data that is accessed less frequently, but requires rapid access when needed."),
                        sidebarLayout(
                          
                          sidebarPanel(
@@ -141,7 +144,7 @@ ui <- fluidPage(
                        )
               ),
               tabPanel("One Zone-IA",br(),
-                       
+                       p("One Zone-Infrequent Access is designed similarly to Infrequent Access, but unlike other storage class which store data in a minimum of three Availability Zones, One Zone-IA stores data in a single Availability Zone."),
                        sidebarLayout(
                          
                          sidebarPanel(
@@ -206,7 +209,7 @@ ui <- fluidPage(
                        )
               ),
               tabPanel("Glacier",br(),
-                       
+                       p("Glacier is a low-cost data archiving solution and provides three retrieval options of varying speed."),
                        sidebarLayout(
                          
                          sidebarPanel(
@@ -278,7 +281,7 @@ ui <- fluidPage(
                        )
               ),
               tabPanel("Glacier-Deep Archive",br(),
-                       
+                       p("Glacier-Deep Archive is designed for long-term data storage that is accessed once or twice in a year with two retrieval speeds to choose from."),
                        sidebarLayout(
                          
                          sidebarPanel(
@@ -350,28 +353,11 @@ ui <- fluidPage(
                        )
               ),
               tabPanel("Storage Class Comparison",br(),
-                       
-                       #fluidRow(column(3,h3("Plot & Table Options") )),
-                       
+                       p("Easily compare the storage, upload, data retrieval, and total cumulative costs of the different storage classes based on the inputs provided."),
                        sidebarLayout(
                          
                          sidebarPanel(
-                           
-                           #fluidRow(column(8,align="center",h4("Storage Costs") )),
-                           #fluidRow(column(3,actionButton("storageShow","Show") ),
-                                    #column(3,actionButton("storageHide","Hide") ),
-                                    #column(3,actionButton("storageOnly","Show Only") )
-                           #),br(),
-                           #fluidRow(column(8,align="center",h4("Data Upload Costs") )),
-                           #fluidRow(column(3,actionButton("uploadShow","Show") ),
-                                    #column(3,actionButton("uploadHide","Hide") ),
-                                    #column(3,actionButton("uploadOnly","Show Only") )
-                           #),br(),
-                           #fluidRow(column(8,align="center",h4("Data Retrieval Costs") )),
-                           #fluidRow(column(3,actionButton("retrievalShow","Show") ),
-                                    #column(3,actionButton("retrievalHide","Hide") ),
-                                    #column(3,actionButton("retrievalOnly","Show Only") )
-                           #),br(),
+    
                            fluidRow(column(8,align="center",h4("Months to View") )),
                            fluidRow(
                              column(8,
@@ -385,11 +371,40 @@ ui <- fluidPage(
                                        tabPanel("Storage",br(),plotOutput("compPlotStorage",width="80%"),br(),tableOutput("compTableStorage")),
                                        tabPanel("Upload",br(),plotOutput("compPlotUpload",width="80%"),br(),tableOutput("compTableUpload")),
                                        tabPanel("Data Retrieval",br(),plotOutput("compPlotDataRetrieval",width="80%"),br(),tableOutput("compTableDataRetrieval"))
-                                       
-                           
+
+                           )
                          )
                        )
+                     ),
+              
+              tabPanel("Instructions",br(),
+                       
+                       p("The following instructions explain how to use the cost calculator in further detail."),
+                       h4("Total Amount of Storage"),
+                       tags$li("Input the total amount of storage being uploaded into S3"),
+                       tags$li("Select the storage unit (GB, TB, or PB)"),
+                       h4("Total Number of Objects"),
+                       tags$li("Input the total number of objects/files that make up the amount of storage inputted"),
+                       h4("Data Retrieval Amount"),
+                       tags$li("Input the estimated amount of monthly data retrieval"),
+                       tags$li("Select the storage unit (GB, TB, PB, or %)"),
+                       tags$li("Selecting % will calculate the monthly data retrieval amount as a percentage of the total amount of storage"),
+                       h4("Storage Growth"),
+                       tags$li("Input the estimated amount of monthly storage growth"),
+                       tags$li("Select the storage unit (GB, TB, PB, or %)"),
+                       tags$li("Selecting % will calculate the monthly growth amount as a percentage of the total amount of storage"),
+                       h4("Months to View"),
+                       tags$li("Use the slider to indicate how many months to show on the plots and tables"),
+                       tags$li("Choose between 3 and 36 months"),
+                       h4("Copy Inputs"),
+                       tags$li("Once all the inputs are entered for a given storage class, the inputs can be copied over to other storage classes"),
+                       tags$li("Select which storage classes the inputs should be copied to then click the Copy button"),
+                       tags$li("Selecting All will copy the inputs to all other storage classes"),
+                       h4("Retrieval Speed (Glacier & Glacier-Deep Archive Only)"),
+                       tags$li("Select the retrieval speed interested in using for data retrievals"),
+                       tags$li("Defaults to Standard"),
+                       h4("Storage Class Comparison Tab"),
+                       tags$li("Use this tab to easily compare each of the storage classes separated by storage, upload, data retrieval, and cumulative costs")
               )
   )
-)
 )
